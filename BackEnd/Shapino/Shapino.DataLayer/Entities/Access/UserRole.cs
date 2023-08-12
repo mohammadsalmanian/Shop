6b1174c9bfc6fe83b4e65bb28524a4ae06a05f6d
mohammadsalmanian <115922233+mohammadsalmanian@users.noreply.github.com>
2023-08-13 00:09:54 +0430

    BackEnd Comit 1

---
+++ BackEnd/Shapino/Shapino.DataLayer/Entities/Access/UserRole.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Shapino.DataLayer.Entities.Account;
+using Shapino.DataLayer.Entities.Common;
+
+namespace Shapino.DataLayer.Entities.Access
+{
+    public class UserRole:BaseEntity
+    {
+        #region properties
+        public long UserId { get; set; }
+        public long RoleId { get; set; }
+        #endregion
+
+        #region Relations
+        public User User { get; set; }
+        public Role Role { get; set; }
+        #endregion
+    }
+}
---
+++ BackEnd/Shapino/Shapino.WebApi/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Shapino.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, Microsoft.AspNetCore.Hosting.IHostingEnvironment hostingEnvironment)
+        {
+            Configuration = configuration;
+            HostingEnvironment = hostingEnvironment;
+        }
+        public IConfiguration Configuration { get; }
+        public Microsoft.AspNetCore.Hosting.IHostingEnvironment HostingEnvironment { get; }
+        // This method gets called by the runtime. Use this method to add services to the container
+        public void ConfigureServices0(IServiceCollection services)
+        {
+            services.AddSingleton<IConfiguration>(new ConfigurationBuilder().SetBasePath(
+                Directory.GetCurrentDirectory()
+                ).AddJsonFile($"appsettings.json").Build());            
+
+            //services.AddApplicationDbContext(Configuration);
+
+            /*services.AddDbContext<AngularEshopDbContext>(options =>
+            {
+                var connectionString = "ConnectionStrings:AngularEshopConnection:Development";
+                options.UseSqlServer(Configuration[connectionString]);
+            });*/
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
