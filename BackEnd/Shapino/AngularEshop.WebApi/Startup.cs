105aebc3e297d14d5f80b48366328021accbeec1
mohammadsalmanian <115922233+mohammadsalmanian@users.noreply.github.com>
2023-09-02 22:55:50 +0430

    Commit_Base_Union

---
+++ BackEnd/Shapino/AngularEshop.WebApi/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using AngularEshop.Core.Utilities.Extensions.Connection;
+using AngularEshop.DataLayer.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AngularEshop.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, Microsoft.AspNetCore.Hosting.IHostingEnvironment hostingEnvironment)
+        {
+            Configuration = configuration;
+            HostingEnvironment = hostingEnvironment;
+        }
+
+        public IConfiguration Configuration { get; }
+        public Microsoft.AspNetCore.Hosting.IHostingEnvironment HostingEnvironment { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IConfiguration>(
+                new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile($"appsettings.json")
+                    .Build()
+            );
+
+            services.AddApplicationDbContext(Configuration);
+
+            /*services.AddDbContext<AngularEshopDbContext>(options =>
+            {
+                var connectionString = "ConnectionStrings:AngularEshopConnection:Development";
+                options.UseSqlServer(Configuration[connectionString]);
+            });*/
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
