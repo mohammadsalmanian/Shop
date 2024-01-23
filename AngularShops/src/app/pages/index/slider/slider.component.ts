b400ad01157d83a2d6ebb0e12736c26772d6faa6
mohammadsalmanian <115922233+mohammadsalmanian@users.noreply.github.com>
2024-01-23 23:51:21 +0330

    commit front

---
+++ AngularShops/src/app/pages/index/slider/slider.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+import {SliderService} from '../../../services/slider.service';
+import {Slider} from '../../../DTOs/Sliders/Slider';
+
+@Component({
+  selector: 'app-index-slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.scss']
+})
+export class SliderComponent implements OnInit {
+
+  private sliders: Slider[] = [];
+
+  constructor(
+    private sliderService: SliderService
+  ) {
+  }
+
+  ngOnInit(): void {
+
+    this.sliderService.getCurrentSliders().subscribe(sliders => {
+      if (sliders === null) {
+        this.sliderService.GetSliders().subscribe(res => {
+          if (res.status === 'Success') {
+            this.sliderService.setCurrentSliders(res.data);
+          }
+        });
+      } else {
+        this.sliders = sliders;
+      }
+      console.log(this.sliders);
+    });
+  }
+}
---
+++ AngularShops/src/app/services/slider.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {HomeSliderResponse} from '../DTOs/Sliders/HomeSliderResponse';
+import {Slider} from '../DTOs/Sliders/Slider';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SliderService {
+
+  private homeSliders: BehaviorSubject<Slider[]> = new BehaviorSubject<Slider[]>(null);
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  public GetSliders(): Observable<HomeSliderResponse> {
+    return this.http.get<HomeSliderResponse>('https://localhost:44381/slider/GetActiveSliders');
+  }
+
+  public getCurrentSliders(): Observable<Slider[]> {
+    return this.homeSliders;
+  }
+
+  public setCurrentSliders(sliders: Slider[]) {
+    this.homeSliders.next(sliders);
+  }
+}
